570ebfb2d75164a2edcd50d5d4e73e3dec61ba76
harshita <harshita1225@gmail.com>
2022-10-21 11:16:05 +0200

    task completed

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
   const [data, setData] = useState({
@@ -6,15 +7,47 @@ export const Login = () => {
     password: "r3@ct",
   });
 
+  const [inputData, setInputData] = useState({
+    userName: "",
+    password: "",
+    submit: false,
+  });
+
+  const navigate = useNavigate();
+
+  const changeHandler = (e) => {
+    setInputData({ ...inputData, [e.target.name]: e.target.value });
+  };
+  const formHandler = (e) => {
+    e.preventDefault();
+    if (
+      inputData.userName === data.userName &&
+      inputData.password === data.password
+    ) {
+      navigate("/home");
+    } else {
+      setInputData({ ...inputData, submit: true });
+    }
+  };
+
   return (
     <>
       <h1>Login</h1>
-      <p>The username or password is incorrect!</p>
-      <form>
-        <input type="text" name="userName" placeholder="Username" />
+      {inputData.submit && <p>The username or password is incorrect!</p>}
 
-        <input type="password" name="password" placeholder="Password" />
-
+      <form onSubmit={formHandler}>
+        <input
+          onChange={changeHandler}
+          type="text"
+          name="userName"
+          placeholder="Username"
+        />
+        <input
+          onChange={changeHandler}
+          type="password"
+          name="password"
+          placeholder="password"
+        />
         <input type="submit" value="Submit" />
       </form>
     </>
